a3c637048e18788aedadc33b9b9a3d3c6290549b
swarup4 <swarup.saha004@hotmail.com>
2022-05-27 04:03:45 +0530

    Add Edit Project functionality

--- modules/project/controller.js
+++ modules/project/controller.js
@@ -26,6 +26,20 @@ router.post("/addProject", (req, res) => {
     });
 });
 
+router.put("/editProject/:id", (req, res) => {
+    let id = req.params.id;
+
+    Project.findOneAndUpdate({ _id: id }, req.body, {
+        timestamps: { createdAt: false, updatedAt: true }
+    }, (err, project) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(project);
+        }
+    })
+});
+
 router.post('/upload', upload.array('image', 1), (req, res) => {
     res.send({ image: req.file });
 });
